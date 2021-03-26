b3154fcf16ab0744c9ec97ec30aab64cadb41391
Corbin Crutchley <crutchcorn@gmail.com>
2021-03-26 12:15:11 -0700

    Initial work to get files from GitHub

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// noop
+const { Octokit } = require("@octokit/core");
+
+const octokit = new Octokit();
+
+async function main() {
+    const {data} = await octokit.request("GET /gists/{gist_id}", {
+      gist_id: "004066b2225c7f1093c7e249cabfcdf5",
+    });
+
+    const {files} = data;
+
+    // If one file, mark that as selected
+    // If more than one file, ask which file to import
+
+    console.log(files["index.rs"]);
+}
+
+main()
+    .catch(console.error);
