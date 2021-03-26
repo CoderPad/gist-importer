949be1ef11292cc79604508308fc8274651650c8
Corbin Crutchley <crutchcorn@gmail.com>
2021-03-26 12:29:23 -0700

    Added early CoderPad API example

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { Octokit } = require("@octokit/core");
+const fetch = require('node-fetch');
 
 const octokit = new Octokit();
 
-async function main() {
+require("dotenv").config();
+
+async function getGist() {
     const {data} = await octokit.request("GET /gists/{gist_id}", {
       gist_id: "004066b2225c7f1093c7e249cabfcdf5",
     });
@@ -13,6 +16,23 @@ async function main() {
     // If more than one file, ask which file to import
 
     console.log(files["index.rs"]);
+}
+
+const CODERPAD_KEY = process.env.CODERPAD_API;
+
+async function main() {
+    const url = "https://app.coderpad.io/api/pads?sort=updated_at,desc";
+
+    const res = await fetch(url, {
+        // method: "post",
+        // body: JSON.stringify(body),
+        headers: {
+            "Authorization": `Token token="${CODERPAD_KEY}"`
+         },
+    })
+    .then((res) => res.json())
+
+    console.log(res);
 }
 
 main()
