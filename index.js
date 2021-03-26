8acad258477a21082a7814434e26b5a9c747398b
Corbin Crutchley <crutchcorn@gmail.com>
2021-03-26 13:03:37 -0700

    Added gist input + validation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { Octokit } = require("@octokit/core");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 const inquirer = require("inquirer");
 
 require("dotenv").config();
@@ -21,8 +21,33 @@ const octokit = new Octokit();
  * @returns {GistFile}
  */
 async function getGist() {
+  const gistRegex = /https:\/\/gist.github.com\/.*\/([0-9a-f]{32}$)/;
+
+  const gistQ = await inquirer.prompt([
+    {
+      type: "input",
+      name: "gist_url",
+      message: "Enter the URL or ID for the Gist you want to import",
+      validate: function (value) {
+        var pass = value.match(gistRegex);
+        if (pass) return true;
+        
+        const validId = value.match(/^[0-9a-f]{32}$/);
+        if (validId) return true;
+
+        return "Please enter a valid Gist URL or ID";
+      },
+    },
+  ]);
+
+  const gistURL = gistQ["gist_url"];
+
+  let [_, gistId] = gistRegex.exec(gistURL) || [];
+
+  if (!gistId) gistId = gistURL;
+
   const { data } = await octokit.request("GET /gists/{gist_id}", {
-    gist_id: "137ec7db36f98cf6c2aabe0a1890f95a",
+    gist_id: gistId,
   });
 
   const { files } = data;
@@ -57,32 +82,30 @@ async function getGist() {
 const CODERPAD_KEY = process.env.CODERPAD_API;
 
 /**
- * @param {GistFile} selectedFile 
+ * @param {GistFile} selectedFile
  */
 async function getCoderPad(selectedFile) {
-    const url = "https://app.coderpad.io/api/pads";
+  const url = "https://app.coderpad.io/api/pads";
 
-    const res = await fetch(url, {
-      method: "post",
-      body: JSON.stringify({
-        // title: '',
-        language: selectedFile["language"].toLowerCase(),
-        contents: selectedFile["content"],
-      }),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Token token="${CODERPAD_KEY}"`,
-      },
-    }).then((res) => res.json());
+  const res = await fetch(url, {
+    method: "post",
+    body: JSON.stringify({
+      // title: '',
+      language: selectedFile["language"].toLowerCase(),
+      contents: selectedFile["content"],
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Token token="${CODERPAD_KEY}"`,
+    },
+  }).then((res) => res.json());
 
-    console.log(res);
+  console.log(res);
 }
-
 
 async function main() {
   const selectedFile = await getGist();
   await getCoderPad(selectedFile);
 }
 
-main()
-    .catch(console.error);
+main().catch(console.error);
